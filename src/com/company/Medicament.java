03aae23a7c1904ca7235ea7a03da03419349d5e2
Alexis-Pel <alexispelissier2002@gmail.com>
2020-11-24 22:22:21 +0100

    class medicament

--- src/com/company/Medicament.java
+++ src/com/company/Medicament.java
@@ -1,4 +1,19 @@
 package com.company;
 
 public class Medicament {
+    private String ISNCode;
+    private String nomenclature;
+    private String description;
+    private int quantityAvailable;
+    private String dateOfPrescription;
+    private int Price;
+
+    public Medicament(String ISNCode, String nomenclature, String description, int quantityAvailable, String dateOfPrescription, int price) {
+        this.ISNCode = ISNCode;
+        this.nomenclature = nomenclature;
+        this.description = description;
+        this.quantityAvailable = quantityAvailable;
+        this.dateOfPrescription = dateOfPrescription;
+        Price = price;
+    }
 }
