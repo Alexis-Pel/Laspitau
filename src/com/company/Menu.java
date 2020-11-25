e994e1b2b78d265eb6fc80358ee4043ccf8cb9bf
Alexis-Pel <alexispelissier2002@gmail.com>
2020-11-25 09:02:39 +0100

    menu

--- src/com/company/Menu.java
+++ src/com/company/Menu.java
@@ -7,16 +7,24 @@ public class Menu {
         Scanner scanner = new Scanner(System.in);
         System.out.println("---- Bienvenue dans le logiciel du groupe Laspitau ----\n");
         System.out.println("Choisissez votre option :");
-        System.out.println("1 : Ajouter Patient ");
+        System.out.println("1 : Administration ");
+        System.out.println("2 : Entertainment");
         int input = scanner.nextInt();
         try{
             switch (input){
                 case 1:
-                    Patient.addPatient();
+                    System.out.println("\n -- Administration du groupe -- \n");
+                    System.out.println("Choisissez votre option :");
+                    System.out.println("1 : Ajouter Patient");
+                    input = scanner.nextInt();
+                    switch (input){
+                        case 1:
+                            Patient.addPatient();
+                            break;
+                    }
                     break;
                 case 2:
-                    break;
-                case 3:
+                    System.out.println("EN CONSTRUCTION");
                     break;
             }
         } catch (Exception e) {
