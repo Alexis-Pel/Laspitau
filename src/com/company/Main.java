58578dde95440c452e3bfe85ccc63506ad029154
Alexis-Pel <alexispelissier2002@gmail.com>
2020-11-26 16:13:50 +0100

    ajout de la classe log

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,12 @@
 package com.company;
+import java.io.FileNotFoundException;
+import java.io.UnsupportedEncodingException;
+
 public class Main {
 
-    public static void main(String[] args) {
-        Semaine.createDays();
-	    Menu.menu();
+    public static void main(String[] args) throws FileNotFoundException, UnsupportedEncodingException {
+        Logger log = new Logger("Nouveau log");
+        //Semaine.createDays();
+	    //Menu.menu();
     }
 }
