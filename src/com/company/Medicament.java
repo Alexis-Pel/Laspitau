0fed037d66520a0a2a72dbce73588f9ba462213a
Shadow <kcoudray2001@gmail.com>
2020-11-26 12:13:07 +0100

    Ajout de la liste de médicaments

--- src/com/company/Medicament.java
+++ src/com/company/Medicament.java
@@ -1,4 +1,7 @@
 package com.company;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
 public class Medicament {
     private String ISNCode;
@@ -6,14 +9,38 @@ public class Medicament {
     private String description;
     private int quantityAvailable;
     private String dateOfPrescription;
-    private int Price;
+    private int price;
+    public static List<Medicament> listeMedicament = new ArrayList<>();
 
-    public Medicament(String ISNCode, String nomenclature, String description, int quantityAvailable, String dateOfPrescription, int Price) {
+    public Medicament(String ISNCode, String nomenclature, String description, int quantityAvailable, String dateOfPrescription, int price) {
         this.ISNCode = ISNCode;
         this.nomenclature = nomenclature;
         this.description = description;
         this.quantityAvailable = quantityAvailable;
         this.dateOfPrescription = dateOfPrescription;
-        this.Price = Price;
+        this.price = price;
+    }
+
+    public static void addMedicament(){
+        System.out.println("Ajouter combien de médicaments ?");
+        Scanner scanner = new Scanner(System.in);
+        int input = scanner.nextInt();
+        for(int i = 0; i<input ; i++){
+            Scanner scanner1 = new Scanner(System.in);
+            System.out.println("Code ISN du médicament");
+            String ISNCode = scanner1.nextLine();
+            System.out.println("Nomenclature du médicament");
+            String nomenclature = scanner1.nextLine();
+            System.out.println("Description du médicament (quelques mots)");
+            String description = scanner1.nextLine();
+            System.out.println("Quantité disponible");
+            int quantityAvailable = scanner1.nextInt();
+            System.out.println("Date de la prescription");
+            String dateOfPrescrpition = scanner1.nextLine();
+            System.out.println("Prix du médicament");
+            int price = scanner1.nextInt();
+            Medicament medicament = new Medicament(ISNCode,nomenclature,description,quantityAvailable,dateOfPrescrpition,price);
+            listeMedicament.add(medicament);
+        }
     }
 }
