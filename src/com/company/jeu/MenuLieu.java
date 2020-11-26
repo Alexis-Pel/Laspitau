b801a6d4f6f1d840a8ffccc16036256ca04b943b
Alexis-Pel <alexispelissier2002@gmail.com>
2020-11-26 12:08:23 +0100

    JavaDoc

--- src/com/company/jeu/MenuLieu.java
+++ src/com/company/jeu/MenuLieu.java
@@ -2,6 +2,9 @@ package com.company.jeu;
 
 public class MenuLieu {
 
+    /***
+     * L'introduction du jeu
+     */
     public static void menuLieu(){
         System.out.println("Vous êtes chez vous devant votre ordinateur en plein cours Java.");
         System.out.println("Vous fermais les les yeux et soudainement :");
